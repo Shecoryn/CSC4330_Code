ad0445771697c4c9cdd15a851fcbbb3b37113645
Shecory'n Porter <43757945+Shecoryn@users.noreply.github.com>
2020-02-07 22:23:47 -0500

    Add files via upload

---
+++ Homework1.java
@@ -0,0 +1,16 @@
+import java.io.File; 
+import java.util.StringTokenizer;
+import java.util.Scanner;
+public class Homework1 {
+
+	public static void main(String[] args) throws Exception 
+	{
+		//File directory for file input
+		File file = new File("C:\\Users\\sheco\\eclipse-workspace\\Programming_Language_Concepts\\PLC_HW1.txt"); 
+		Scanner sc = new Scanner(file);
+		StringTokenizer thisFile = new StringTokenizer(file.toString());
+		while (sc.hasNextLine()) 
+			System.out.println(sc.nextLine());
+	}
+
+}
